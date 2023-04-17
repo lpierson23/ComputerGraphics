89ba2522ad6ed11f9f30f6b8d0a4d3dfe7152063
lpierson23 <lpierson@nd.edu>
2023-04-17 16:56:37 -0400

    Base code changes

--- chessGame.js
+++ chessGame.js
@@ -5,70 +5,6 @@ var gl;
 
 var program;
 
-var u_TextureSamplerLoc;
-
-var numVertices  = 6;
-
-var texSize = 256;
-var numChecks = 64;
-
-//var flag = true;
-
-var near = 0.3;
-var far = 3.0;
-
-var  fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
-var  aspect = 1.0;       // Viewport aspect ratio
-
-var texture1;
-
-// Create a checkerboard pattern using floats
-
-var image1 = new Uint8Array(4*texSize*texSize);
-
-    for ( var i = 0; i < texSize; i++ ) {
-        var c;
-        for ( var j = 0; j <texSize; j++ ) {
-            var patchx = Math.floor(i/(texSize/numChecks));
-            var patchy = Math.floor(j/(texSize/numChecks));
-            if(patchx%2 ^ patchy%2) c = 255;
-            else c = 0;
-            //c = 255*(((i & 0x8) == 0) ^ ((j & 0x8)  == 0))
-            image1[4*i*texSize+4*j] = c;
-            image1[4*i*texSize+4*j+1] = c;
-            image1[4*i*texSize+4*j+2] = c;
-            image1[4*i*texSize+4*j+3] = 255;
-        }
-    }
-
-
-var pointsArray = [];
-var colorsArray = [];
-var texCoordsArray = [];
-
-var texCoord = [
-    vec2(0, 0),
-    vec2(0, 1),
-    vec2(1, 1),
-    vec2(1, 0)
-];
-
-var zmin = 1.0;
-var zmax = 50.0;
-
-var vertices = [
-    vec4( -5.5, 0.0,  zmax, 1.0 ),
-    vec4( -5.5,  0.0,  zmin, 1.0 ),
-    vec4( 5.5, 0.0,  zmin, 1.0 ),
-    vec4( 5.5,  0.0,  zmax, 1.0 ),
-];
-
-var modelViewMatrix, projectionMatrix;
-var u_modelViewMatrixLoc, u_projectionMatrixLoc;
-var eye = vec3(0.0, 10.0, 0.0);
-var at = vec3(0.0, 0.0, (zmax+zmin)/4);
-const up = vec3(0.0, 1.0, 0.0);
-
 var vertexColors = [
     vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
     vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
@@ -77,51 +13,10 @@ var vertexColors = [
     vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
     vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
     vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-
     vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
 ];
 
 window.onload = init;
-
-function configureTexture(image) {
-    texture1 = gl.createTexture();
-    gl.bindTexture( gl.TEXTURE_2D, texture1 );
-    // we do not need this, as the texture is procedurally generated
-    //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0,
-        gl.RGBA, gl.UNSIGNED_BYTE, image);
-    gl.generateMipmap( gl.TEXTURE_2D );
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
-           gl.NEAREST);
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
-};
-
-function quad(a, b, c, d) {
-
-     pointsArray.push(vertices[a]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[0]);
-
-     pointsArray.push(vertices[b]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[1]);
-
-     pointsArray.push(vertices[c]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[2]);
-
-     pointsArray.push(vertices[a]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[0]);
-
-     pointsArray.push(vertices[c]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[2]);
-
-     pointsArray.push(vertices[d]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[3]);
-}
 
 
 function init() {
@@ -139,8 +34,6 @@ function init() {
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    quad( 1, 0, 3, 2 );
-
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
@@ -157,73 +50,64 @@ function init() {
     gl.vertexAttribPointer( a_vPositionLoc, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(a_vPositionLoc);
 
-    configureTexture(image1);
-
-    var tBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW );
-    
-    var a_vTexCoordLoc = gl.getAttribLocation( program, "a_vTexCoord");
-    gl.vertexAttribPointer(a_vTexCoordLoc, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(a_vTexCoordLoc);
-
-    gl.activeTexture( gl.TEXTURE0 );
-    u_TextureSamplerLoc = gl.getUniformLocation(program, "u_TextureSampler");
-    gl.uniform1i(u_TextureSamplerLoc, 0);
-
     u_modelViewMatrixLoc = gl.getUniformLocation( program, "u_modelViewMatrix" );
     u_projectionMatrixLoc = gl.getUniformLocation( program, "u_projectionMatrix" );
-
-// sliders for viewing parameters
-
-    document.getElementById("zFarSlider").onchange = function(event) {
-        far = event.target.value;
-    };
-    document.getElementById("zNearSlider").onchange = function(event) {
-        near = event.target.value;
-    };
-    document.getElementById("aspectSlider").onchange = function(event) {
-        aspect = event.target.value;
-    };
-    document.getElementById("fovSlider").onchange = function(event) {
-        fovy = event.target.value;
-    };
-
-        document.getElementById("Texture Style").onclick = function( event) {
-          switch(event.target.index) {
-            case 0:
-               gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
-                   gl.NEAREST_MIPMAP_NEAREST);
-               gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER,
-                    gl.NEAREST );
-               break;
-            case 1:
-               gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
-                    gl.NEAREST_MIPMAP_LINEAR);
-                gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER,
-                    gl.LINEAR );
-               break;
-           case 2:
-                gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
-                    gl.LINEAR_MIPMAP_NEAREST );
-                gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER,
-                    gl.NEAREST );
-               break;
-            case 3:
-                gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
-                    gl.LINEAR_MIPMAP_LINEAR );
-                gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER,
-                    gl.LINEAR );
-               break;
-        };
-    };
 
     render();
 }
 
+function drawKing() {
+
+}
+
+function drawQueen() {
+
+}
+
+function drawBishop() {
+
+}
+
+function drawKnight() {
+
+}
+
+function drawRook() {
+
+}
+
+function drawPawn() {
+
+}
+
+function drawPieces() {
+
+}
+
+function drawBoard() {
+
+}
+
+function setUpBoard() {
+
+}
+
+function playBook() {
+    //king
+
+    //queen
+
+    //bishop
+
+    //knight
+
+    //rook
+
+    //pawn
+}
+
 var render = function() {
     gl.clear( gl.COLOR_BUFFER_BIT);
-
 
     modelViewMatrix = lookAt(eye, at , up);
     projectionMatrix = perspective(fovy, aspect, near, far);
